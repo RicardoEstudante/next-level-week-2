7a42375e17d896a74103afb56ab309ca4ee1517f
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2020-08-05 19:08:42 -0300

    finish teacher item component

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css'
+
+function TeacherItem() {
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src="https://avatars1.githubusercontent.com/u/42756551?s=460&u=e4475ddca2f9c544b67ea619c636d50512d54be0&v=4" alt="Ricardo Carvalho"/>
+                <div>
+                    <strong>Ricardo Carvalho</strong>
+                    <span>Química</span>
+                </div>
+            </header>
+
+            <p>
+                Entusiasta das melhores tecnologias de química avançada.
+                <br/><br/>
+                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+            </p>
+            
+            <footer>
+                <p>
+                    Preço/Hora
+                    <strong>R$ 100,00</strong>
+                </p>
+                <button>
+                    <img src={whatsappIcon} alt="Whatsapp"/>
+                    Entrar em contato
+                </button>
+            </footer>
+        </article>
+    )
+}
+
+export default TeacherItem;
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import './styles.css'
 
 function TeacherList() {
@@ -26,32 +26,10 @@ function TeacherList() {
             </PageHeader>
 
             <main>
-                <article className="teacher-item">
-                    <header>
-                        <img src="https://avatars1.githubusercontent.com/u/42756551?s=460&u=e4475ddca2f9c544b67ea619c636d50512d54be0&v=4" alt="Ricardo Carvalho"/>
-                        <div>
-                            <strong>Ricardo Carvalho</strong>
-                            <span>Química</span>
-                        </div>
-                    </header>
-
-                    <p>
-                        Entusiasta das melhores tecnologias de química avançada.
-                        <br/><br/>
-                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-                    </p>
-                    
-                    <footer>
-                        <p>
-                            Preço/Hora
-                            <strong>R$ 100,00</strong>
-                        </p>
-                        <button>
-                            <img src={whatsappIcon} alt="Whatsapp"/>
-                            Entrar em contato
-                        </button>
-                    </footer>
-                </article>
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
             </main>
         </div>
     )
