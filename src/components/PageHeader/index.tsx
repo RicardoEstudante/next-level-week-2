ab114dc62ccbf17d704849d5fb48866c69632bef
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2020-08-05 16:42:09 -0300

    finish header css

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -5,8 +5,13 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
+import './styles.css';
 
-function PageHeader() {
+interface PageHeaderProps {
+    title: string,
+}
+
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
     return (
         <header className="page-header">
             <div className="top-bar-container">
@@ -16,8 +21,12 @@ function PageHeader() {
             </div>
 
             <div className="header-content">
-                <strong>Estes são os proffys disponíveis.</strong>
+                <strong>{props.title}</strong>
+
+                {props.children}
             </div>
+
+            
         </header>
     )
 }
