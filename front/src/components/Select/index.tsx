cf6022c38d8669c234c20962703f827d4d3def34
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2020-08-11 21:06:21 -0300

    finish select component

--- front/src/components/Select/index.tsx
+++ front/src/components/Select/index.tsx
@@ -15,9 +15,10 @@ interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
     return (
         <div className="select-block">
             <label htmlFor={name}>{label}</label>
-            <select id={name} {...rest} >
+            <select id={name} {...rest}>
+                <option value="" disabled selected hidden>Selecione uma opção</option>
                 {options.map(option => {
-                    return <option value={option.value}>{option.label}</option>
+                    return <option key={option.value} value={option.value}>{option.label}</option>
                 })}
             </select>
         </div>
