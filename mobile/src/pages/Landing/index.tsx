9941dad3188d611d2dd0ae3180516270d6eefbb1
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2020-08-21 01:19:03 -0300

    searching total connections from api

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { View, Image, Text , TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+
+import api from '../../services/api';
 
 import styles from './styles';
 
@@ -11,6 +13,16 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
 function Landing() {
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('connections').then(Response => {
+            const { total} = Response.data
+
+            setTotalConnections(total);
+        })
+    },[]);
+
     const { navigate } = useNavigation();
 
     function handleNavigateToGiveClassesPage() {
@@ -45,7 +57,7 @@ function Landing() {
             </View>
 
             <Text style={styles.totalConnections}>
-                Total de 50 conexões realizadas {' '}
+                Total de {totalConnections} conexões realizadas {' '}
                 <Image source={heartIcon}/>
             </Text>
         </View>
