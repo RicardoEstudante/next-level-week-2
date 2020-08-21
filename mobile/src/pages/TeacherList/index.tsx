00fed8726ec5c4da0ce5346f832747ee22a7884c
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2020-08-21 03:17:59 -0300

    finish favorites

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, ScrollView, Text, TextInput } from 'react-native';
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import TeacherItem, { Teacher } from '../TeacherItem';
 import PageHeader from '../PageHeader';
@@ -13,17 +14,33 @@ import styles from './styles';
 
 function TeacherList() {
     const [teachers, setTeachers] = useState([]);
+    const [favorites, setFavorites] = useState<number[]>([]);
     const [isFiltersVisble, setIsFiltersVisble] = useState(false);
 
     const [subject, setSubject] = useState('');
     const [week_day, setWeekDay] = useState('');
     const [time, setTime] = useState('');
 
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then(response => {
+            if(response) {
+                const favoritedTeachers = JSON.parse(response);
+                const favoritedTeachersIds = favoritedTeachers.map((teacher: Teacher) => {
+                    return teacher.id;
+            })
+            
+            setFavorites(favoritedTeachersIds);
+            }
+        });
+    }
+
     function handleToggleFiltersVisible(){
         setIsFiltersVisble(!isFiltersVisble);
     }
 
     async function handleFiltersSubmit(){
+        loadFavorites();
+
         const response = await api.get('classes', {
             params: {
                 subject,
@@ -94,8 +111,13 @@ function TeacherList() {
                 }}
             >
                 {teachers.map((teacher: Teacher) => {
-                    return <TeacherItem key={teacher.id} teacher={teacher} />
-                })}
+                    return (
+                        <TeacherItem 
+                            key={teacher.id} 
+                            teacher={teacher}
+                            favorited={favorites.includes(teacher.id)}
+                        />)
+                    })}
             </ScrollView>
         </View>
     );
